572202ebfa620d4b1294b3924a36e9e6c78f8910
xhlazz <160189425+xhlazz@users.noreply.github.com>
2025-03-08 22:40:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-const musicLinks = [
-    'https://cdn.discordapp.com/attachments/1114702060147974318/1348129370186977390/spotidownloader.com_-_wym__-_Conico.mp3?ex=67ce56d0&is=67cd0550&hm=79ae386c03eea49ad0c647ec0f63c0297ebd57a9c7c1672a566dd9657e3bca72&',
-    'https://cdn.discordapp.com/attachments/1114702060147974318/1348129390113853512/spotidownloader.com_-_TKIEROCONMIGO_-_Cesarvsthewrld.mp3?ex=67ce56d5&is=67cd0555&hm=31e1f5b1d24060434e50e3a574175e62ceec63909fbf528660e39affa9668534&',
-    'https://cdn.discordapp.com/attachments/1114702060147974318/1348129401417629717/spotidownloader.com_-_Tell_me_-_novyhuman.mp3?ex=67ce56d8&is=67cd0558&hm=9d99c4ee792246a5d980dfc774071c02712715c3ff2a5c30917faba95170f04f&',
-    'https://cdn.discordapp.com/attachments/1114702060147974318/1348129413404954634/spotidownloader.com_-_teconozcomasydtisoyadicto_-_Is_Dead.mp3?ex=67ce56db&is=67cd055b&hm=9b7466e0a75c3d7f84ec7c928a3e9e6f8133ab1976689322b049507bee4ba537&',
-    'https://cdn.discordapp.com/attachments/1114702060147974318/1348129427527041075/spotidownloader.com_-_Sin_Conexion_-_Young_Dune.mp3?ex=67ce56de&is=67cd055e&hm=f00eff72d7b5e4b5ec1ab523503d91770fa01aaf4edd9f170cfd85c822d53bb7&',
-    'https://cdn.discordapp.com/attachments/1114702060147974318/1348129439267164280/spotidownloader.com_-_Show_Me_How
+const musicFiles = [
+    "https://raw.githubusercontent.com/xhlazz/Everything-About-xhlazz/main/spotidownloader.com%20-%20Leni%20(Crystal%20Castles%20vs%20GoodBooks)%20-%20GoodBooks.mp3",
+    "https://raw.githubusercontent.com/xhlazz/Everything-About-xhlazz/main/spotidownloader.com%20-%20Tell%20me%20-%20novyhuman.mp3",
+    "https://raw.githubusercontent.com/xhlazz/Everything-About-xhlazz/main/spotidownloader.com%20-%20TKIEROCONMIGO%20-%20Cesarvsthewrld.mp3",
+    "https://raw.githubusercontent.com/xhlazz/Everything-About-xhlazz/main/spotidownloader.com%20-%20Sin%20Conexion%20-%20Young%20Dune.mp3",
+    "https://raw.githubusercontent.com/xhlazz/Everything-About-xhlazz/main/spotidownloader.com%20-%20Show%20Me%20How%20-%20Men%20I%20Trust.mp3"
+];
+
+let currentMusicIndex = Math.floor(Math.random() * musicFiles.length);
+const backgroundMusic = document.getElementById('background-music');
+
+backgroundMusic.src = musicFiles[currentMusicIndex];
+backgroundMusic.play();
+
+function toggleMusic() {
+    const toggleButton = document.getElementById('toggle-music');
+    if (backgroundMusic.paused) {
+        backgroundMusic.play();
+        toggleButton.innerHTML = 'Stop Music';
+    } else {
+        backgroundMusic.pause();
+        toggleButton.innerHTML = 'Play Music';
+    }
+}
+
+function changeMusic() {
+    currentMusicIndex = (currentMusicIndex + 1) % musicFiles.length;
+    backgroundMusic.src = musicFiles[currentMusicIndex];
+    backgroundMusic.play();
+    document.getElementById('toggle-music').innerHTML = 'Stop Music';
+}
+
+function toggleSection(sectionId, button) {
+    const section = document.getElementById(sectionId);
+    const arrow = button.querySelector('.arrow');
+    if (section.style.display === 'none' || section.style.display === '') {
+        section.style.display = 'block';
+        arrow.classList.add('arrow-up');
+    } else {
+        section.style.display = 'none';
+        arrow.classList.remove('arrow-up');
+    }
+}
+
+function calculateAge(birthDate) {
+    const now = new Date();
+    const age = now.getFullYear() - birthDate.getFullYear();
+    const
